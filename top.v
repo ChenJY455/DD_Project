2b2e3effb539116a9414b3061edd1cbb0a96f4a3
Wan <3220102682@zju.edu.cn>
2024-01-06 22:11:44 +0800

    显示部分基本完成

--- top.v
+++ top.v
@@ -2,93 +2,130 @@
 //DD_final_project
 //2023/12/25
 module top(
-    //时钟
-    input clk,  // 50MHz
+    input clk,                             // 50MHz
+    input [15:0] SW,                       // switch
+    //input reset,                           //negative signal
 
     //...
 
 
 
     //VGA输出信息
-    output  vga_hsync,
-    output  vga_vsync,
-    output [3:0] vga_r,
-    output [3:0] vga_g,
-    output [3:0] vga_b
-    //output wire [9:0] col_addr,         //屏幕y坐标
-    //output wire [8:0] row_addr           //屏幕x坐标
+    output  HS,
+    output  VS,
+    output [3:0] R,
+    output [3:0] B,
+    output [3:0] G
     );
-
-    //棋盘信息 5种颜�???
-    //0 is red, 1 is green, 2 is blue, 3 is yellow, 4 is purple�?? 5 is black
-    reg [2:0] chess_board [7:0][7:0];   // 2D array of 8x8 each element is 3 bits
-    reg [11:0] colour_red = 12'hF00;
-    reg [11:0] colour_green = 12'h0F0;
-    reg [11:0] colour_blue = 12'h00F;
-    reg [11:0] colour_yellow = 12'hFF0;
-    reg [11:0] colour_purple = 12'hF0F;
-    reg [9:0] chess_width = 640 / 8;
-    reg [8:0] chess_height = 480 / 8;
-    
-    //棋盘信息初始�??? 第一排红色，第二排绿色，第三排蓝色，第四排黄色，第五排紫色，其余为黑�???
-    integer i, j;
+    //jie mian
+    reg cover;                  //gamestart
+    reg finish;                 //gameover
     initial begin
-        for (i = 0; i < 8; i=i+1) begin
-            for (j = 0; j < 8; j=j+1) begin
-                if (i == 0) begin
-                    chess_board[i][j] = 3'b000;
-                end else if (i == 1) begin
-                    chess_board[i][j] = 3'b001;
-                end else if (i == 2) begin
-                    chess_board[i][j] = 3'b010;
-                end else if (i == 3) begin
-                    chess_board[i][j] = 3'b011;
-                end else if (i == 4) begin
-                    chess_board[i][j] = 3'b100;
-                end else begin
-                    chess_board[i][j] = 3'b101;
-                end
-            end
-        end
-    end 
-    
-
-
-
-    //VGA
-    //时钟分频
-    wire [31:0] div_res;
-    vga_clkdiv vga_clkdiv_inst(
-        .clk(clk),
-        .rst(1'b1),
-        .div_res(div_res)
-    );
-
-    // 输出VGA信息，输出VGA信号
-    wire [11:0] vga_data;
-    reg [11:0] vga_data_reg;
-    integer x; integer y;                        //将屏幕x坐标，屏幕y坐标转化为棋盘坐�??
-    always @ (*) begin
-        //size 480*640 -> 8*8
-        x = col_addr - 80 >= 0 ? (col_addr - 80) / chess_height : 0;
-        y = row_addr / chess_width;
-        vga_data_reg = chess_board[x][y];
+        cover=0;
+        finish=0;
     end
+    //时钟分�
+    wire [31:0]clkdiv;
+    wire clk_in_game;
+    wire clk_cover;
+    assign clk_in_game = clk & cover;
+    assign clk_cover = clk & (~cover) & (~finish);
+    
+    clkdiv clkdiv_inst(
+        .clk(clk),
+        .rst(1'b0),
+        .clkdiv(clkdiv)
+    );
+	
+    reg [11:0] vga_data;
+    wire [9:0]  col_addr;
+    wire [8:0]  row_addr;
 
-    assign vga_data = vga_data_reg;
-    vgac v0 (
-        .vga_clk(div_res[1]),
-        .clrn(1'b1),
+    vgac v0(
+        .vga_clk(clkdiv[1]),
+        .clrn(SW[0]),
         .d_in(vga_data),
         .row_addr(row_addr),
         .col_addr(col_addr),
-        .r(vga_r),
-        .g(vga_g),
-        .b(vga_b),
-        //.rdn(1'b1),
-        .hsync(vga_hsync),
-        .vsync(vga_vsync)        
+        .r(R),
+        .g(G),
+        .b(B),
+        .hs(HS),
+        .vs(VS)
     );
+    
+    //chess board
+    //0 for red, 1 for green, 2 for blue, 3 for yellow, 4 for purple, 5 for white
+    reg [2:0] chess_board[7:0][7:0];
+    integer i,j,k;
+    initial begin
+        k=0;
+        for (i=0;i<8;i=i+1) begin
+            for (j=0;j<8;j=j+1) begin
+                chess_board[i][j]=k;
+                k = (k+1)%6;
+            end
+        end
+    end
 
+    reg [18:0] pic1_addr;
+    wire [11:0] pic1;                            //cover's data
+    ////////////////// IP CORE  /////////////
+    cover cover_inst(
+        .clka   (clkdiv[1]),     
+        .addra  (pic1_addr),      
+        .douta  (pic1)       
+    );
+    ////////////////// IP CORE's INPUT ////////////////
+    always @(posedge clk_cover) begin
+        pic1_addr <= (col_addr>=0 && col_addr<=639 && row_addr>=0 && row_addr <= 479) ? row_addr * 640 + col_addr:0; 
+    end
+    
+    ///////////////////DISPLAY PART/////////////////////
+    
+    always @(posedge clk) begin
+        if (cover == 0) begin
+            if (col_addr>=0 && col_addr<=639 && row_addr>=0 && row_addr <= 479)
+                vga_data <= pic1; 
+            //if(col_addr>=253&&col_addr<=398&&row_addr>=251&&row_addr<=255)      //spark
+              //  vga_data<=12'b0001_0001_0011; 
+        end
+        else begin                              //in game
+            if (col_addr < 120 || col_addr > 519 || row_addr < 40 || row_addr > 439)
+            vga_data <= 12'b1111_1111_1111;
+            else begin
+                i <= (row_addr - 40)/50;
+                j <= (col_addr - 120)/50;
+                if (chess_board[i][j] == 0) begin
+                    vga_data <= 12'b1111_0000_0000;
+                end
+                else if (chess_board[i][j] == 1) begin
+                    vga_data <= 12'b0000_1111_0000;
+                end
+                else if (chess_board[i][j] == 2) begin
+                    vga_data <= 12'b0000_0000_1111;
+                end
+                else if (chess_board[i][j] == 3) begin
+                    vga_data <= 12'b1111_1111_0000;
+                end
+                else if (chess_board[i][j] == 4) begin
+                    vga_data <= 12'b1111_0000_1111;
+                end
+                else if (chess_board[i][j] == 5) begin
+                    vga_data <= 12'b1111_1111_1111;
+            end
+            end
+        end  
+    end
+    
+    ////////////////control signal/////////////////////
+    always @(posedge clk) begin
+        if (SW[1] == 0) begin
+            cover <= 0;
+        end
+        else begin
+            cover <= 1;
+        end
+    end
 
 endmodule
